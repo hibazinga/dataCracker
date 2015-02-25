30d9ba80340388e0d587c3f4983d30403dfaf6dc
Yanbin Ren <ybren@YanbinsMacBookPro.local>
2015-02-25 00:09:31 -0800

    matrix

--- dataCracker/matrix.h
+++ dataCracker/matrix.h
@@ -3,7 +3,7 @@
 //  dataCracker
 //
 //  Created by Yanbin Ren on 2/24/15.
-//  Copyright (c) 2015 Yanbin Ren. All rights reserved.
+//  Copyright (c) 2015 Data Crackers. All rights reserved.
 //
 
 #ifndef dataCracker_matrix_h
@@ -15,16 +15,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+
+void matrix_add(double** A, double** B, double** ans,int m,int n);
+void matrix_substract(double** A, double** B, double** ans,int m,int n);
+void matrix_times(double c, double** A, double** ans, int m,int n);
+void matrix_mutiply(double** A, double** B, double** ans,int m,int k,int n);
+void write_matrix_to_file(double **A,int m,int n);
+void read_matrix_from_file(double **A,int m,int n,FILE *fp);
+int get_column(FILE *fp);
+int get_row(char* s);
+char* readline(FILE* f);
 
 
-// A+B
+// ans=A+B
 void matrix_add(double** A, double** B, double** ans,int m,int n){
+    int i,j;
+    for (i=0; i<m; i++) {
+        for (j=0; j<n; j++) {
+            ans[i][j]=A[i][j]+B[i][j];
+        }
+    }
+}
 
+// ans=A-B
+void matrix_substract(double** A, double** B, double** ans,int m,int n){
+    int i,j;
+    for (i=0; i<m; i++) {
+        for (j=0; j<n; j++) {
+            ans[i][j]=A[i][j]-B[i][j];
+        }
+    }
+}
 
+// ans=c*A
+void matrix_times(double c, double** A, double** ans, int m,int n){
+    int i,j;
+    for (i=0; i<m; i++) {
+        for (j=0; j<n; j++) {
+            ans[i][j]=A[i][j]*c;
+        }
+    }
 }
 
 
-
+// ans=A*B
 // M*K  *  K*N  =   M*N
 void matrix_mutiply(double** A, double** B, double** ans,int m,int k,int n){
     int i1,i2;
@@ -48,12 +83,23 @@ void write_matrix_to_file(double **A,int m,int n){
     for (i=0; i<m; ++i) {
         for (j=0; j<n; ++j) {
             fprintf(fp, "%lf", A[i][j]);
-            fputc(',',fp);
+            fputc('\t',fp);
         }
         fputc('\n', fp);
     }
 }
 
+
+
+// '\t' seperated
+void read_matrix_from_file(double **A,int m,int n,FILE *fp){
+
+
+
+}
+
+
+//initialize matrix with random 0/1
 void random_initialize(double **A,int m,int n){
     srand((unsigned)time(NULL));
     int i,j;
@@ -62,4 +108,40 @@ void random_initialize(double **A,int m,int n){
             A[i][j]=RAND_MAX/rand();
         }
     }
+}
+
+
+int get_column(FILE *fp){
+    int column=1;
+    char *line=readline(fp);
+    while (*line!='\0' && *line!='\n' && *line != '\r' && *line !=EOF) {
+        if(*line=='\t') column++;
+        line++;
+    }
+    fclose(fp);
+    return column;
+}
+
+int get_row(char *filename){
+    FILE * fp;
+    char buffer[10];
+    fp=popen(strcat("wc -l ",filename),"r");
+    fgets(buffer,sizeof(buffer),fp);
+    pclose(fp);
+    int row= atoi(buffer);
+    return row;
+}
+
+char* readline(FILE* f)
+{
+    char* line = (char*) calloc(1, sizeof(char) );
+    char c;
+    int len = 0;
+    while ( (c = fgetc(f) ) != EOF && c != '\n')
+    {
+        line = (char*) realloc(line, sizeof(char) * (len + 2) );
+        line[len++] = c;
+        line[len] = '\0';
+    }
+    return line;
 }
